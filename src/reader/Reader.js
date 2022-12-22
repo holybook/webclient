af867e480f8210be541ce92c3c4ad9477b67fe9a
Hannes Widmoser <widmoser@gmail.com>
2022-12-22 07:10:00 +0100

    Fix autosizing for virtual rendering

--- src/reader/Reader.js
+++ src/reader/Reader.js
@@ -1,35 +1,50 @@
-import { InfiniteLoader, List } from 'react-virtualized';
+import {AutoSizer, CellMeasurer, CellMeasurerCache, InfiniteLoader, List} from 'react-virtualized';
 import 'react-virtualized/styles.css';
 import {useLoaderData} from "react-router-dom"; // only needs to be imported once
 
+const cache = new CellMeasurerCache({
+    defaultHeight: 60,
+    fixedWidth: true
+});
 const list = [];
 
-function isRowLoaded ({ index }) {
+function isRowLoaded({index}) {
     return !!list[index];
 }
 
-function rowRenderer({ key, index, style }) {
-    if (!isRowLoaded({ index })) {
+function rowRenderer({key, index, parent, style}) {
+    if (!isRowLoaded({index})) {
         return <div key={key}>Loading...</div>
     }
 
     return (
-        <div
+        <CellMeasurer
+            cache={cache}
+            columnIndex={0}
             key={key}
-            style={style}>
-            {list[index].text}
-        </div>
+            overscanRowCount={10}
+            parent={parent}
+            rowIndex={index}
+        >
+            <div
+                key={key}
+                style={style}>
+                {list[index].text}
+            </div>
+        </CellMeasurer>
     );
 }
 
 export function Reader() {
     const book = useLoaderData();
-    return <Paragraphs book={book}/>
+    return (<div className="reader">
+        <Paragraphs book={book}/>
+    </div>);
 }
 
-function Paragraphs({ book }) {
+function Paragraphs({book}) {
 
-    function loadMoreRows ({ startIndex, stopIndex }) {
+    function loadMoreRows({startIndex, stopIndex}) {
         return fetch(`http://0.0.0.0:8080/books/${book.id}/paragraphs?start=${startIndex}&end=${stopIndex}`)
             .then(response => {
                 return response.json()
@@ -46,16 +61,19 @@ function Paragraphs({ book }) {
         loadMoreRows={loadMoreRows}
         rowCount={book.paragraphCount}
     >
-        {({ onRowsRendered, registerChild }) => (
-            <List
-                height={200}
-                onRowsRendered={onRowsRendered}
-                ref={registerChild}
-                rowCount={book.paragraphCount}
-                rowHeight={200}
-                rowRenderer={rowRenderer}
-                width={300}
-            />
+        {({onRowsRendered, registerChild}) => (
+            <AutoSizer>
+                {({width, height}) => (
+                    <List
+                        height={height}
+                        onRowsRendered={onRowsRendered}
+                        ref={registerChild}
+                        rowCount={book.paragraphCount}
+                        rowHeight={cache.rowHeight}
+                        rowRenderer={rowRenderer}
+                        width={width}
+                    />)}
+            </AutoSizer>
         )}
     </InfiniteLoader>
 }
