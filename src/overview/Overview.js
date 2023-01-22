903bf1b0ec95ba79d5f22438cbc76697685b6e4d
Hannes Widmoser <widmoser@gmail.com>
2023-01-22 23:24:18 +0100

    Filter out books that are not available in a language

--- src/overview/Overview.js
+++ src/overview/Overview.js
@@ -5,41 +5,49 @@ import './Overview.scss';
 import {getSupportedLanguages} from "../common/Utils";
 
 export function Overview() {
-  const books = useLoaderData();
-  const [params,] = useSearchParams();
-  const language = params.get('lang') ?? 'en';
+    const books = useLoaderData();
+    const [params,] = useSearchParams();
+    const language = params.get('lang') ?? 'en';
 
-  const bookItems = getBookItemsByLanguage(books, language);
+    const bookItems = getBookItemsByLanguage(books, language);
 
-  return (
-      <div className="overview">
-        <TopBar supportedLanguages={getSupportedLanguages(books)}
-                activeLanguage={language}/>
-        <div id="content">
-          {bookItems.map(
-              item =>
-                  <BookItem title={item.title} language={item.language}
-                            id={item.id} key={item.id}
-                  />)}
+    return (
+        <div className="overview">
+            <TopBar supportedLanguages={getSupportedLanguages(books)}
+                    activeLanguage={language}/>
+            <div id="content">
+                {bookItems.map(
+                    item =>
+                        <BookItem title={item.title} language={item.language}
+                                  id={item.id} key={item.id}
+                        />)}
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
 function getBookItemsByLanguage(books, language) {
-  return books.map(book => {
-    return getBookItemByLanguage(book, language);
-  }).sort((a, b) => a.title.localeCompare(b.title))
+    return books.map(book => {
+        return getBookItemByLanguage(book, language);
+    }).filter((item) => item !== null).sort((a, b) => a.title.localeCompare(b.title))
 }
 
 function getBookItemByLanguage(book, language) {
-  return {
-    id: book.id,
-    language: language,
-    title: findTitle(book.translations, language)
-  };
+    const title = findTitle(book.translations, language);
+    if (title === null) {
+        return null;
+    }
+    return {
+        id: book.id,
+        language: language,
+        title: title
+    };
 }
 
 function findTitle(translations, language) {
-  return translations.find(t => t.language === language).title;
+    const translation = translations.find(t => t.language === language);
+    if (translation === undefined) {
+        return null;
+    }
+    return translation.title;
 }
